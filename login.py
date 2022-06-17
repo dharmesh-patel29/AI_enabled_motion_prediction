51ff6f1a153295b6599db63238e27442e81f65e4
Dharmesh <dppatelrh29@gmail.com>
2022-06-18 00:25:30 +0200

    Add three switches

--- login.py
+++ login.py
@@ -1,59 +1,58 @@
 # Creating App
-
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.core.window import Window
-from kivy.properties import StringProperty
-from kivy.utils import get_color_from_hex
 from kivy.core.text import LabelBase
 from kivymd.uix.behaviors import FakeRectangularElevationBehavior
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivy.uix.widget import Widget
-
-    
-
-from kivymd.uix.behaviors import RoundedRectangularElevationBehavior
+from kivymd.uix.screen import MDScreen
 from kivymd.uix.card import MDCard
 
-Window.size= (310,580)
-
-
+Window.size= (320,510)
 
 class LoginScreen(Screen):
     pass
 
-
 class SignupScreen(Screen):
     pass
 
-
 class MainScreen(Screen):
-    
     pass
 
+class ActivityGraphScreen(Screen):
+    pass
+
+class DailyGraph(Screen):
+    pass
+
+class MonthlyGraph(Screen):
+    pass
+
+class YearlyGraph(Screen):
+    pass
+
+class Setting(Screen):
+    pass
 
 sm = ScreenManager()
 sm.add_widget(LoginScreen(name='login screen'))
 sm.add_widget(SignupScreen(name='signup-screen'))
 sm.add_widget(MainScreen(name='mainscreen'))
+sm.add_widget(ActivityGraphScreen(name="activitiygraphscreen"))
+sm.add_widget(DailyGraph(name='dailygraph'))
+sm.add_widget(MonthlyGraph(name='monthlygraph'))
+sm.add_widget(YearlyGraph(name='yearlygraph'))
+sm.add_widget(Setting(name='setting'))
 
 class ProfileCard(MDFloatLayout, FakeRectangularElevationBehavior):
-    
     pass
-
-class Separator(Widget):
-    pass
-
 
 class MyApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Light"
         return Builder.load_file('myapp.kv')
-
-
-        
-
 
 if __name__ == "__main__":
     MyApp().run()
